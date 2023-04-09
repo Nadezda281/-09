470322f5bb9630f55e08821d932e72ecc2bfc897
Nadezda <nadenka.merkulowa@yandex.ru>
2023-04-09 23:28:48 +0500

    решение задачи 6

---
+++ задача2/задача4/задача6/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите A: ");
+string text1 = Console.ReadLine();
+int number = Convert.ToInt32(text1);
+
+if (number% 2==0)
+
+
+{
+    Console.WriteLine("number1 четное");
+}
+else
+{
+    Console.WriteLine("number1 не четное");
+}
