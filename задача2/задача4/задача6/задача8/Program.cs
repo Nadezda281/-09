6b64e8e3e9a0e276c62cf666a9909eb0b040f5ae
Nadezda <nadenka.merkulowa@yandex.ru>
2023-04-10 00:36:54 +0500

    задача8

---
+++ задача2/задача4/задача6/задача8/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число: ");
+string text = Console.ReadLine();
+int N = Convert.ToInt32(text);
+
+
+int i=0;
+while (i<N) 
+
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+    
+    i=i+1;
+}
+;
