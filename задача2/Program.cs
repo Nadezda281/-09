450fd4107b6c14c620fecf0d2879172cd4a7335b
Nadezda <nadenka.merkulowa@yandex.ru>
2023-04-09 21:25:51 +0500

    Решение задачи 2

---
+++ задача2/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.Write("Введите A: ");
+string text1 = Console.ReadLine();
+int number1 = Convert.ToInt32(text1);
+Console.Write("Введите B: ");
+string text2 = Console.ReadLine();
+int number2 = Convert.ToInt32(text2);
+
+
+if (number1 > number2)
+{
+    Console.WriteLine("A это max,B это min ");
+}
+else
+{
+    Console.WriteLine("A это min,B это max ");
+};
+
+
